d8600d59ed097ca060a693052bf13e9cdc5fab38
Viktor Tanyi <tanyi.viktor@gmail.com>
2018-10-02 19:44:36 +0200

    Add routing

--- src/main/typescript/index.tsx
+++ src/main/typescript/index.tsx
@@ -1,13 +1,22 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 
 import AccountPage from "./AccountPage"
 import TransactionPage from "./TransactionPage"
 
 ReactDOM.render(
-  <div>
-    <AccountPage />
-    <TransactionPage />
-  </div>,
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/accounts">Accounts</Link></li>
+        <li><Link to="/transactions">Transactions</Link></li>
+      </ul>
+      <hr />
+      <Route exact path="/" component={AccountPage} />
+      <Route path="/accounts" component={AccountPage} />
+      <Route path="/transactions" component={TransactionPage} />
+    </div>
+  </Router>,
   document.getElementById("root")
 )
